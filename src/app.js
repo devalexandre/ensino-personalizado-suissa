19722e851cafcf6408e6fd58864b92e79ed25b20
devalexandre <alexandre@indev.net.br>
2017-07-20 15:57:51 -0300

    start

---
+++ src/app.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Toolbar from './components/navbar/toolbar'
+
+import './app.styl'
+
+export default class App extends React.Component {
+  render() {
+    return (<Toolbar/>)
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,14 @@
+'use strict'
+
+import React from 'react'
+import ReactDOM from 'react-dom';
+import Main from './app.js'
+
+ReactDOM.render(
+    <Main />,
+    document.querySelector('[data-js="app"]')
+)
+
+
+
+

2a7c0699018f1e8d9ed88f659db7760583c6a4cc
devalexandre <alexandre@indev.net.br>
2017-09-14 15:04:25 -0300

    toolbar com buttom para mobile

--- src/components/navbar/toolbar.js
+++ src/components/navbar/toolbar.js
@@ -1,21 +1,40 @@
 import React from 'react'
 
 class Toolbar extends React.Component {
-    
-    render() {
-        return (
-            <nav className='molecule-toolbar' >
-                <ul className='atom-navbar'>
-                    <li className='atom-navbar-title'><a href=''>O Trovador</a></li>
-                    <ul className='atom-navbar-group'>
-                    <li className='atom-navbar-item'><a href=''>Home</a></li>
-                    <li className='atom-navbar-item'><a href=''>About</a></li>
-                    <li className='atom-navbar-item'><a href=''>contact</a></li>
-                    </ul>
-                </ul>
-            </nav>
-        )
+
+  constructor(){
+    super()
+
+    this.state = {
+      menubar:false
     }
+  }
+  render () {
+    return (
+      <nav className='molecule-toolbar'>
+        <button className='atom-toolbar-control'>
+          <i className="fa fa-bars" aria-hidden="true"></i>
+        </button>  
+
+        <ul className='atom-navbar'>
+          <li className='atom-navbar-title'>
+            <a href=''>O Trovador</a>
+          </li>
+          <ul className='molecule-navbar-items'>
+            <li className='atom-navbar-item'>
+              <a href=''>Home</a>
+            </li>
+            <li className='atom-navbar-item'>
+              <a href=''>About</a>
+            </li>
+            <li className='atom-navbar-item'>
+              <a href=''>contact</a>
+            </li>
+          </ul>
+        </ul>
+      </nav>
+    )
+  }
 }
 
 export default Toolbar

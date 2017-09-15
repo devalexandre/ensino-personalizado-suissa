8f36dbfc5eac5b355a12d6ff17ac7b5fa62cfd64
devalexandre <alexandre@indev.net.br>
2017-09-14 21:59:26 -0300

    error no toolbar.styl

--- src/components/navbar/toolbar.js
+++ src/components/navbar/toolbar.js
@@ -2,35 +2,30 @@ import React from 'react'
 
 class Toolbar extends React.Component {
 
-  constructor(){
+  constructor () {
     super()
 
     this.state = {
-      menubar:false
+      menubar: false
     }
   }
   render () {
     return (
       <nav className='molecule-toolbar'>
         <button className='atom-toolbar-control'>
-          <i className="fa fa-bars" aria-hidden="true"></i>
-        </button>  
-
-        <ul className='atom-navbar'>
-          <li className='atom-navbar-title'>
-            <a href=''>O Trovador</a>
+          <i className='fa fa-bars' aria-hidden='true'></i>
+        </button>
+        <h3 className='atom-navbar-title'><a href=''>O Trovador</a></h3>
+        <ul className='molecule-navbar-items'>
+          <li className='atom-navbar-item'>
+            <a href=''>Home</a>
           </li>
-          <ul className='molecule-navbar-items'>
-            <li className='atom-navbar-item'>
-              <a href=''>Home</a>
-            </li>
-            <li className='atom-navbar-item'>
-              <a href=''>About</a>
-            </li>
-            <li className='atom-navbar-item'>
-              <a href=''>contact</a>
-            </li>
-          </ul>
+          <li className='atom-navbar-item'>
+            <a href=''>About</a>
+          </li>
+          <li className='atom-navbar-item'>
+            <a href=''>contact</a>
+          </li>
         </ul>
       </nav>
     )
